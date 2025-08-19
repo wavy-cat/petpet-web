ec41db436cca1811062a3a83e6b9d45c3bac8958
WavyCat <me@wavycat.ru>
2025-08-19 23:00:34 +0500

    del test

--- src/pages/ds/[id].ts
+++ src/pages/ds/[id].ts
@@ -4,12 +4,8 @@ export const GET: APIRoute = async ({ request, locals }) => {
     const { env } = locals.runtime;
     const token = env.IAM_TOKEN;
     const base = env.CONTAINER_ENDPOINT;
-    console.log(base);
-    console.log(request);
     const source = new URL(request.url);
     const url = new URL(source.pathname + source.search, base);
-    console.log(url);
-    console.log(url.toString());
     const resp = await fetch(
         url.toString(),
         {
@@ -19,7 +15,6 @@ export const GET: APIRoute = async ({ request, locals }) => {
             }
         }
     );
-    console.log(resp.url);
     return resp;
 }
 
