4af10f82339d4a9e82eb491b03d76200d80df22f
WavyCat <me@wavycat.ru>
2025-08-20 21:51:15 +0500

    feat: change copy button and remove sonner

--- src/components/buttons/CopyButton.tsx
+++ src/components/buttons/CopyButton.tsx
@@ -1,29 +1,42 @@
 "use client"
 
-import { toast } from "sonner";
+import { useState } from "react";
 import { Button } from '@/components/ui/button';
-import { Copy } from 'lucide-react';
+import { Copy, Check } from 'lucide-react';
 
 interface Props {
   url: string | null;
 }
 
 export default function CopyButton({ url }: Props) {
+  const [isCopied, setIsCopied] = useState(false);
+
   const copyClick = () => {
     if (url) {
-      navigator.clipboard.writeText(url).catch((err) => {
+      navigator.clipboard.writeText(url).then(() => {
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 1200);
+      }).catch((err) => {
         console.error('Failed to copy the text:', err);
-      })
-      toast("Link copied!", {
-        closeButton: true,
       });
-    };
+    }
   };
 
   return (
     <Button disabled={!url} size="sm" className="cursor-pointer" onClick={copyClick}>
-      <Copy />
-      Copy Link
+      {isCopied ? (
+        <>
+          <Check />
+          Copied!
+        </>
+      ) : (
+        <>
+          <Copy />
+          Copy Link
+        </>
+      )}
     </Button>
   );
 }
