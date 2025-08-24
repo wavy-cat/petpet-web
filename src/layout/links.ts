13a715ea89a3a1408ec71cfa30ef582ea61110fe
WavyCat <me@wavycat.ru>
2025-08-24 20:43:48 +0500

    rename privacy links

--- src/layout/links.ts
+++ src/layout/links.ts
@@ -2,5 +2,5 @@ import type { Link } from "../components/header/types";
 
 export const links: Link[] = [
 	{ url: "https://github.com/wavy-cat/petpet-go", title: "GitHub", newtab: true },
-	{ url: "/policy", title: "Privacy Policy" },
+	{ url: "/policy", title: "Privacy" },
 ];
