739b7c5a8bc7e75aa3f6780cab717994eb549585
WavyCat <me@wavycat.ru>
2025-10-04 11:54:15 +0500

    refactor: layouts

---
+++ src/layout/types.ts
@@ -0,0 +1,5 @@
+export interface BaseLayoutProps {
+    title: string;
+    description?: string;
+    pageFull?: boolean;
+}
--- src/links.ts
+++ src/links.ts
@@ -1,6 +1,6 @@
-import type { Link } from "../components/header/types";
+import type { Link } from "./components/header/types";
 
 export const links: Link[] = [
 	{ url: "https://github.com/wavy-cat/petpet-go", title: "GitHub", newtab: true },
-	{ url: "/policy", title: "Privacy" },
+	{ url: "/privacy", title: "Privacy" },
 ];
