0ad46d2e8f902c7b2147eb8c1eeda8566c06e791
WavyCat <root@wavycat.me>
2026-02-13 07:35:52 +0500

    Reapply "Update site URL in astro.config.mjs"
    This reverts commit 434555955c69f65ecd0f4d40c681d8caaf615542.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pet.wavycat.me',
+  site: 'https://pet.wavycat.me/app',
   output: 'server',
 
   vite: {
