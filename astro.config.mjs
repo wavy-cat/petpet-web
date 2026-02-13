c5c0308f9a7d609e49a13a04f6c5b1bbbb25cf36
WavyCat <root@wavycat.me>
2026-02-13 07:36:06 +0500

    Revert "chore: update `astro.config.mjs` with `base` and `trailingSlash` options"
    This reverts commit f6c1dd5f33a82a078ce770c5abb0fc7a9321b059.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/app',
-  trailingSlash: 'never',
   site: 'https://pet.wavycat.me/app',
   output: 'server',
 
